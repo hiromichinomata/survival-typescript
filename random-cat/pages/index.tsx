b7dc5f37d640adb3598092ad23e996a19fc15268
Hiromichi NOMATA <git@hiromichinomata.com>
2023-05-20 22:22:35 +0900

    update: server side rendering

--- random-cat/pages/index.tsx
+++ random-cat/pages/index.tsx
@@ -1,16 +1,14 @@
-import { NextPage } from "next";
-import { useEffect, useState } from "react";
+import { GetServerSideProps, NextPage } from "next";
+import { useState } from "react";
+import styles from "./index.module.css";
 
-const IndexPage: NextPage = () => {
-  const [imageUrl, setImageUrl] = useState("");
-  const [loading, setLoading] = useState(true);
+type Props = {
+  initialImageUrl: string;
+};
 
-  useEffect(() => {
-    fetchImage().then((newImage) => {
-      setImageUrl(newImage.url); // 画像URLの状態を更新する
-      setLoading(false); // ローディング状態を更新する
-    });
-  }, []);
+const IndexPage: NextPage<Props> = ({ initialImageUrl }) => {
+  const [imageUrl, setImageUrl] = useState(initialImageUrl);
+  const [loading, setLoading] = useState(false);
 
   const handleClick = async () => {
     setLoading(true); // 読込中フラグを立てる
@@ -19,15 +17,28 @@ const IndexPage: NextPage = () => {
     setLoading(false); // 読込中フラグを倒す
   };
   return (
-    <div>
-      <button onClick={handleClick}>他のにゃんこも見る</button>
-      <div>{loading || <img src={imageUrl} />}</div>
+    <div className={styles.page}>
+      <button onClick={handleClick} className={styles.button}>
+        他のにゃんこも見る
+      </button>
+      <div className={styles.frame}>
+        {loading || <img src={imageUrl} className={styles.img} />}
+      </div>
     </div>
   );
 
   return <div>{loading || <img src={imageUrl} />}</div>;
 };
 export default IndexPage;
+
+export const getServerSideProps: GetServerSideProps<Props> = async () => {
+  const image = await fetchImage();
+  return {
+    props: {
+      initialImageUrl: image.url,
+    },
+  };
+};
 
 const fetchImage = async (): Promise<Image> => {
   const res = await fetch("https://api.thecatapi.com/v1/images/search");
